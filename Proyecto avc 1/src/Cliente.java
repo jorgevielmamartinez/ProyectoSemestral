841ada7077a0f4823244388cdcf8fbc1a74e418b
KeikoMoon <jacob.78100@gmail.com>
2026-04-07 16:51:52 -0400

    avance 2 de Pasajero diagrama "falta conectar cliente con no se que"

---
+++ Proyecto avc 1/src/Cliente.java
@@ -0,0 +1,14 @@
+public class Cliente {
+    private String email;
+
+    public Cliente(IdPersona id, Nombre nom, String email){
+
+    }
+    public String getEmail(){
+        return email;
+    }
+
+    public void setEmail(String email){
+        this.email = email;
+    }
+}
