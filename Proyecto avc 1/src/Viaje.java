f6289ad4389fce647e8d83ce2bc03dcf7c017af5
BenjaTellez <benjamin.tellez2501@alumnos.ubiobio.cl>
2026-04-12 20:09:37 -0400

    Clase viaje terminada

--- Proyecto avc 1/src/Viaje.java
+++ Proyecto avc 1/src/Viaje.java
@@ -41,38 +41,34 @@ public class Viaje {
     public Bus getBus() {
         return bus;
     }
+
     public String[][] getAsientos() {
-        if (bus == null) return new String[0][0];
-
-        int totalAsientos = bus.getNroAsientos();
-        String[][] matriz = new String[totalAsientos][2];
-
-        for (int i = 0; i < totalAsientos; i++) {
+        String[][] matriz = new String[bus.getNroAsientos()][2];
+        for (int i = 0; i < bus.getNroAsientos(); i++) {
             matriz[i][0] = String.valueOf(i + 1);
             matriz[i][1] = "Libre";
         }
 
-        for (pasajes)
+        for (Pasaje p : pasajes) {
+            int numAsiento = p.getAsiento();
+            matriz[numAsiento - 1][1] = "Ocupado";
+        }
 
         return matriz;
     }
 
     public void addPasaje(Pasaje pasaje) {
-        if (pasaje != null && !this.pasajes.contains(pasaje)) {
-            this.pasajes.add(pasaje);
-        }
+        this.pasajes.add(pasaje);
     }
 
     public String[][] getListaPasajeros() {
         String[][] lista = new String[pasajes.size()][4];
         for (int i = 0; i < pasajes.size(); i++) {
             Pasajero p = pasajes.get(i).getPasajero();
-            if (p != null) {
-                lista[i][0] = p.getIdPersona() != null ? p.getIdPersona().toString() : "";
-                lista[i][1] = p.getNombrePersona() != null ? p.getnNombrePersona().toString() : "";
-                lista[i][2] = p.getNomPersona() != null ? p.getNomPersona().toString() : "";
-                lista[i][3] = p.getFonoPersona() != null ? p.getFonoPersona().toString() : "";
-            }
+            lista[i][0] = p.getIdPersona().toString();
+            lista[i][1] = p.getNombreCompleto().toString();
+            lista[i][2] = p.getNomContacto().toString();
+            lista[i][3] = p.getFonoContacto();
         }
         return lista;
     }
@@ -82,7 +78,8 @@ public class Viaje {
     }
 
     public int getNroAsientosDisponibles() {
-        if (bus == null) return 0;
-        return bus.getNroAsientos() - pasajes.size();
+        int ocupados = pasajes.size();
+        int totales = bus.getNroAsientos();
+        return totales - ocupados;
     }
 }
