5c1a94b75a13059dd284393b8afef8fee62cc3f1
jorgevielmamartinez <jorgevielmamartinez@gmail.com>
2026-05-23 21:42:18 -0400

    Controlador y excepcion

--- Proyecto avc 1/src/Modelo/Bus.java
+++ Proyecto avc 1/src/Modelo/Bus.java
@@ -10,7 +10,7 @@ public class Bus {
     private int nroAsientos;
     private List<Viaje> viajes;
 
-    public Bus(String patente, int nroAsientos) {
+    public Bus(String patente, int nroAsientos, Empresa empresa) {
         this.patente = patente;
         this.nroAsientos = nroAsientos;
         this.viajes = new ArrayList<>();
